950cc9d68d3f7f6571e7bc5f633bf16098a3c195
ljy1214 <39375118+ljy1214@users.noreply.github.com>
2018-06-07 12:01:34 +0800

    Update model_version1.py

--- LJY/model_version1.py
+++ LJY/model_version1.py
@@ -4,9 +4,10 @@ import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 import gc
 
+
 data = pd.read_csv('/home/apple/NYC-Taxi/first/result.csv')
 
-# NYC longitude and latitude borders 处理时间和经纬度 删掉过于偏僻的地点
+# NYC longitude and latitude borders 处理经纬度 删掉过于偏僻的地点
 data = data[data['trip_duration'] <  data['trip_duration'].quantile(0.999)]
 data = data[data['trip_duration'] <= data['trip_duration'].mean() + 3*data['trip_duration'].std()]
 ep = 0.0001
@@ -32,35 +33,39 @@ feature_cols = ['vendor_id','passenger_count','pickup_longitude',
                 'bearing_cent_p_cent_d']
 #print(data.shape)
 
-#得到特征对应的矩阵，命名为X，
-X = data[feature_cols]
-y = data['trip_duration']
-X_train, X_eval, y_train, y_eval = train_test_split(X,y, test_size=0.2, random_state=2018)
+#train
+y = np.log(data['trip_duration'].values + 1)
+X_train, X_eval, y_train, y_eval = train_test_split(data[feature_cols].values , y , test_size=0.2 , random_state=2018)
 
-#lgbtrain
 lgb_train = lgb.Dataset(X_train, y_train)
-lgb_eval = lgb.Dataset(X_eval.values, y_eval.values, reference = lgb_train)
+lgb_eval = lgb.Dataset(X_eval, y_eval, reference = lgb_train)
+
 params = {'metric': 'rmse',
-          'learning_rate' :1.0, #试过0.02
-          'num_leaves': 1000, #xgb为10 比2^10略小
-          'feature_fraction': 0.9,
-          'bagging_fraction':0.5,
-          #'bagging_freq':5,
-          'min_data_in_leaf': 200,
-          'max_bin':2000}
+          'learning_rate' :1.0,   #试过0.02
+          'num_leaves': 1000,     #xgb为10 比2^10略小
+          'feature_fraction': 0.3,#feature_fraction 每次迭代随机选择30%的数据
+          'bagging_fraction':0.8, #bagging_fraction 随机选择80%数据执行bagging
+          'bagging_freq':5,
+          'min_data_in_leaf': 200} #min_data_in_leaf 一个叶子的最小数据, 用它来处理过度over-fit
+#num_boost_round提升迭代的个数
+#valid_sets 测试数据
+#early_stopping_rounds 早期停止次数 ，假设为100，验证集的误差迭代到一定程度在100次内不能再继续降低，就停止迭代。
+#verbose_eval (可以输入布尔型或数值型)，也要求evals 里至少有 一个元素。如果为True ,则对evals中元素的评估结果会输出在结果中；
 lgb_model = lgb.train(params,
                       lgb_train,
-                      num_boost_round = 40,
-                      valid_sets = lgb_eval,
+                      num_boost_round = 200,#num_boost_round提升迭代的个数
+                      valid_sets = lgb_eval,#valid_sets 测试数据
                       feature_name=feature_cols,
-                      early_stopping_rounds=10,
-                      verbose_eval = 4)
+                      early_stopping_rounds=50,#early_stopping_rounds 早期停止次数 ，
+                                               #假设为50，验证集的误差迭代到一定程度在50次内不能再继续降低，就停止迭代。
+                      verbose_eval = 10)#verbose_eval (可以输入布尔型或数值型)，也要求evals 里至少有一个元素。
+                                        #如果为数字 假设为10，则每隔10个迭代输出一次。
 del lgb_train
 gc.collect()
 
 #check score
-pred1 = lgb_model.predict(X_train.values, num_iteration = lgb_model.best_iteration)
-pred2 = lgb_model.predict(X_eval.values, num_iteration = lgb_model.best_iteration)
+pred1 = lgb_model.predict(X_train, num_iteration = lgb_model.best_iteration)
+pred2 = lgb_model.predict(X_eval, num_iteration = lgb_model.best_iteration)
 rmsle1= (((y_train-pred1)**2).mean())**0.5
 rmsle2 = (((y_eval-pred2)**2).mean())**0.5
 print('train score: {:.4f}   eval score: {:.4f}'.format(rmsle1,rmsle2))
